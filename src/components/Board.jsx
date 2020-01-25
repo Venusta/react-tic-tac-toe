a6f58cb7cd6a58ef35a78a7a9959fa91eb4a4a2b
April <April@Rain>
2020-01-25 10:10:14 +0000

    Dynamic Board size

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Square from "./Square";
@@ -7,30 +8,32 @@ class Board extends Component {
     const { squares, onClick } = this.props;
     return (
       <Square
+        key={`square${i}`}
         value={squares[i]}
         onClick={() => onClick(i)}
       />
     );
   }
 
+  renderGrid() {
+    const { boardSize: { x, y } } = this.props;
+    const grid = [];
+
+    for (let row = 0; row < y; row += 1) {
+      const rowOfCells = [];
+
+      for (let column = 0; column < x; column += 1) {
+        rowOfCells.push(this.renderSquare(row * x + column)); // index
+      }
+      grid.push(<div key={`row${row}`} className="board-row">{rowOfCells}</div>);
+    }
+    return grid;
+  }
+
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {this.renderGrid()}
       </div>
     );
   }
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -5,20 +5,36 @@ import calculateWinner from "../utils/calculateWinner";
 class Game extends Component {
   constructor(props) {
     super(props);
+    const boardSize = { x: 3, y: 3 };
+    const boardArea = boardSize.x * boardSize.y;
     this.state = {
+      boardSize,
       history: [{
-        squares: Array(9).fill(null),
+        squares: Array(boardArea).fill(null),
       }],
       stepNumber: 0,
       xIsNext: true,
     };
   }
 
+  columnRowFromIndex(i) {
+    const { boardSize: { x } } = this.state;
+    const row = Math.floor(i / x) + 1;
+    const column = (i % x) + 1;
+    return { x: column, y: row };
+  }
+
   handleClick(i) {
-    const { history: historyState, stepNumber, xIsNext } = this.state;
+    const {
+      history: historyState, stepNumber, xIsNext,
+    } = this.state;
+
     const history = historyState.slice(0, stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
+
+    const moveLoc = this.columnRowFromIndex(i);
+
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
@@ -26,6 +42,7 @@ class Game extends Component {
     this.setState({
       history: history.concat([{
         squares,
+        moveLoc,
       }]),
       stepNumber: history.length,
       xIsNext: !xIsNext,
@@ -40,13 +57,16 @@ class Game extends Component {
   }
 
   render() {
-    const { history: historyState, stepNumber, xIsNext } = this.state;
+    const {
+      history: historyState, stepNumber, xIsNext, boardSize,
+    } = this.state;
     const history = historyState;
     const current = history[stepNumber];
     const winner = calculateWinner(current.squares);
+
     const moves = history.map((step, move) => {
       const desc = move
-        ? `Go to move #${move}`
+        ? `Go to move #${move} x: ${history[move].moveLoc.x} y: ${history[move].moveLoc.y}`
         : "Go to game start";
       return (
         // eslint-disable-next-line react/no-array-index-key
@@ -64,7 +84,11 @@ class Game extends Component {
     return (
       <div className="game">
         <div className="game-board">
-          <Board squares={current.squares} onClick={(i) => this.handleClick(i)} />
+          <Board
+            boardSize={boardSize}
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
           <div>{status}</div>
