4dbdb349993050fe9b527305c9b12ecd526b856b
April <April@Rain>
2020-02-01 10:58:13 +0000

    Convert class component to functional component

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,42 +1,36 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import Square from "./Square";
 
-class Board extends Component {
-  renderSquare(i) {
-    const { squares, onClick } = this.props;
-    return (
-      <Square
-        key={`square${i}`}
-        value={squares[i]}
-        onClick={() => onClick(i)}
-      />
-    );
-  }
+const Board = ({ squares, onClick, boardSize: { x, y } }) => {
+  const renderSquare = (i) => (
+    <Square
+      key={`square${i}`}
+      value={squares[i]}
+      onClick={() => onClick(i)}
+    />
+  );
 
-  renderGrid() {
-    const { boardSize: { x, y } } = this.props;
+  const renderGrid = () => {
     const grid = [];
 
     for (let row = 0; row < y; row += 1) {
       const rowOfCells = [];
 
       for (let column = 0; column < x; column += 1) {
-        rowOfCells.push(this.renderSquare(row * x + column)); // index
+        rowOfCells.push(renderSquare(row * x + column)); // index
       }
       grid.push(<div key={`row${row}`} className="board-row">{rowOfCells}</div>);
     }
     return grid;
-  }
+  };
 
-  render() {
-    return (
-      <div>
-        {this.renderGrid()}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      {renderGrid()}
+    </div>
+  );
+};
 
 Board.propTypes = {
   squares: PropTypes.arrayOf(PropTypes.string).isRequired,
